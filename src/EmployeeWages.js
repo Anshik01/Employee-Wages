b18f27e0fe83bfbb946c6ad1c0261b84a3469e19
Anshik Jaiswal <anshik0706@gmail.com>
2025-03-05 12:39:48 +0530

    [Anshik] Add. get daily wages added

--- src/EmployeeWages.js
+++ src/EmployeeWages.js
@@ -1,2 +1,26 @@
+// uc1
 const isPresent = Math.random() > 0.5 ? true : false;
+
+// uc2
+const getDailyWage = () => {
+    let empCheck = Math.floor(Math.random() * 3);
+    let empHour = 0;
+
+    switch(empCheck) {
+        case 1:
+            empHour = 4;
+            break;
+        case 2:
+            empHour = 8;
+            break;
+        default:
+            empHour = 0;
+    }
+
+    return empHour * 20;
+}
+
+// uc1
 console.log(isPresent);
+// uc2
+console.log(getDailyWage());
